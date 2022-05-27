7b852f47f6de30a90b0bb20a0bdc3a0b6c949458
Julia Byström <juliavasastan@gmail.com>
2022-05-27 18:51:05 +0200

    Add embedded google maps map using key set up in env

--- src/pages/HittaHit.js
+++ src/pages/HittaHit.js
@@ -1,15 +1,17 @@
-
+import {GetfotenMap} from "./../components/GetfotenMap"
 export function HittaHit() {
-    return (
-      <main className="container">
-        <section className="section text-section">
-          <h1>Hitta hit</h1>
-          <p>
-                    Kordinater till Getfoten: <br/>
-                    59°22'46.7"N 18°21'29.2"E
-          </p>
-        </section>
-      </main>
-    );
-  }
-  
+   
+  return (
+    <main className="container">
+      <section className="section text-section">
+        <h1>Hitta hit</h1>
+        <p>
+          Kordinater till Getfoten: <br />
+          59°22'46.7"N 18°21'29.2"E
+        </p>
+        <GetfotenMap />
+      </section>
+    </main>
+  );
+}
+
