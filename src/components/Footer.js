1e7a60cc1496eb6669ca613909c0cb9e13c4a955
Julia Byström <juliavasastan@gmail.com>
2023-05-01 15:22:06 +0200

    remove cordinates link changing colour after click

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -49,6 +49,7 @@ export function Footer() {
           target="_blank"
           rel="noreferrer"
           href="https://www.google.com/maps/place/Getfoten+Sj%C3%B6krog/@59.3796416,18.3559171,17z/data=!4m13!1m7!3m6!1s0x0:0xe8adfdb7f9405971!2zNTnCsDIyJzQ2LjciTiAxOMKwMjEnMjkuMiJF!3b1!8m2!3d59.3796389!4d18.3581111!3m4!1s0x465f81a67052c577:0x7d2be9b30a0b7c24!8m2!3d59.3797121!4d18.3571832"
+          className="cordinates-link"
         >
           59°22'46.7"N 18°21'29.2"E
         </a>
